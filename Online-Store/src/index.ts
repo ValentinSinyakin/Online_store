bf41e45e7e0fef76049fe249b9929ec11589e486
ValentinSinyakin <valentinbrest95@gmail.com>
2022-07-18 23:42:56 +0300

    feat: new class button for css file

--- Online-Store/src/index.ts
+++ Online-Store/src/index.ts
@@ -80,358 +80,28 @@ slider2.noUiSlider.on('update', function(values: number, handle: number){
 
 /* ----------------------карточки---------------- */
 
+const ROOT_PRODUCTS=document.getElementById('card');
+class Products {
+ /*  constructor() {
+    this.classNameActive = 'products-element__btn_active';
+    this.labelAdd = 'Добавить в корзину';
+    this.labelRemove = 'Удалить из корзины';
+}
 
-/* document.querySelector('.content').innerHTML = `<div class = "card"></div>`
-for(key in data) {
-     let row = document.createElement('div')
-    row.innerHTML =`<h1>${key}</h1>`
-    document.querySelector('.card').appendChild(row);
-} 
-for( let i=0; i<data[key].lenght;i++){
-    let row = document.createElement("p")
-    row.innerHTML = `<p>${data}[key]</p>`
-    document.querySelector('.card').appendChild(row);
+handlerSetLocatStorage(element, id) {
+    const { pushProduct, products } = localStorageUtil.putProducts(id);
+
+    if (pushProduct) {
+        element.classList.add(this.classNameActive);
+        element.innerText = this.labelRemove;
+    } else {
+        element.classList.remove(this.classNameActive);
+        element.innerText = this.labelAdd;
+    }
+
+    headerPage.render(products.length);
 } */
 
-/* const data2 = {
-   "number1": {
-      "num": '1',
-      "name": 'Большой шар с рисунком',
-      "count": '2',
-      "year": '1960',
-      "shape": 'шар',
-      "color": 'желтый',
-      "size": 'большой',
-      "favorite": false,
-    },
-    "number2": {
-        "num": '2',
-        "name": 'Большой шар с рисунком2',
-        "count": '2',
-        "year": '19602',
-        "shape": 'шар2',
-        "color": 'желтый2',
-        "size": 'большой2',
-        "favorite": false,
-      },
- 
-}
-
-let out = '';
-for(let key in data2){
-    out += 'Название ' + data2[key].name + '<br>';
-}
-document.getElementById('card')?.innerHTML = out;
-
-
-console.log(data2); 
- */
-const ROOT_PRODUCTS=document.getElementById('card');
-/* const data = [
-    {
-      num: '1',
-      name: 'Большой шар с рисунком',
-      count: '2',
-      year: '1960',
-      shape: 'шар',
-      color: 'желтый',
-      size: 'большой',
-      favorite: false,
-    },
-    {
-      num: '2',
-      name: 'Зелёный шар с цветами',
-      count: '5',
-      year: '2000',
-      shape: 'шар',
-      color: 'зелёный',
-      size: 'большой',
-      favorite: false,
-    },
-    {
-      num: '3',
-      name: 'Красный матовый шар',
-      count: '3',
-      year: '1990',
-      shape: 'шар',
-      color: 'красный',
-      size: 'большой',
-      favorite: false,
-    },
-    {
-      num: '4',
-      name: 'Сосулька красная',
-      count: '2',
-      year: '1980',
-      shape: 'фигурка',
-      color: 'красный',
-      size: 'большой',
-      favorite: false,
-    },
-    {
-      num: '5',
-      name: 'Красный виноград',
-      count: '4',
-      year: '1980',
-      shape: 'фигурка',
-      color: 'красный',
-      size: 'средний',
-      favorite: true,
-    },
-    {
-      num: '6',
-      name: 'Красный шар с рисунком',
-      count: '6',
-      year: '2010',
-      shape: 'шар',
-      color: 'красный',
-      size: 'большой',
-      favorite: false,
-    },
-    {
-      num: '7',
-      name: 'Молочно-белый шар',
-      count: '12',
-      year: '1960',
-      shape: 'шар',
-      color: 'белый',
-      size: 'средний',
-      favorite: true,
-    },
-    {
-      num: '8',
-      name: 'Красный шар',
-      count: '10',
-      year: '2010',
-      shape: 'шар',
-      color: 'красный',
-      size: 'большой',
-      favorite: false,
-    },
-    {
-      num: '9',
-      name: 'Колокольчик старинный',
-      count: '2',
-      year: '1950',
-      shape: 'колокольчик',
-      color: 'белый',
-      size: 'большой',
-      favorite: false,
-    },
-    {
-      num: '10',
-      name: 'Белый шар ретро',
-      count: '7',
-      year: '1960',
-      shape: 'шар',
-      color: 'белый',
-      size: 'большой',
-      favorite: false,
-    },
-    {
-      num: '11',
-      name: 'Шишка еловая белая',
-      count: '11',
-      year: '1960',
-      shape: 'шишка',
-      color: 'белый',
-      size: 'малый',
-      favorite: false,
-    },
-    {
-      num: '12',
-      name: 'Белый шар с цветами',
-      count: '5',
-      year: '1980',
-      shape: 'шар',
-      color: 'белый',
-      size: 'большой',
-      favorite: false,
-    },
-    {
-      num: '13',
-      name: 'Шар расписной Река',
-      count: '3',
-      year: '1970',
-      shape: 'шар',
-      color: 'синий',
-      size: 'большой',
-      favorite: true,
-    },
-    {
-      num: '14',
-      name: 'Шар расписной Деревня',
-      count: '4',
-      year: '1970',
-      shape: 'шар',
-      color: 'синий',
-      size: 'большой',
-      favorite: true,
-    },
-    {
-      num: '15',
-      name: 'Колокольчик расписной',
-      count: '3',
-      year: '1970',
-      shape: 'колокольчик',
-      color: 'синий',
-      size: 'средний',
-      favorite: false,
-    },
-    {
-      num: '16',
-      name: 'Шишка расписная Пейзаж',
-      count: '3',
-      year: '1970',
-      shape: 'шишка',
-      color: 'синий',
-      size: 'средний',
-      favorite: true,
-    },
-    {
-      num: '17',
-      name: 'Шишка расписная',
-      count: '7',
-      year: '1970',
-      shape: 'шишка',
-      color: 'красный',
-      size: 'средний',
-      favorite: false,
-    },
-    {
-      num: '18',
-      name: 'Желтый шар с бантом',
-      count: '2',
-      year: '2010',
-      shape: 'шар',
-      color: 'желтый',
-      size: 'большой',
-      favorite: false,
-    },
-    {
-      num: '19',
-      name: 'Желтый шар с паетками',
-      count: '12',
-      year: '1980',
-      shape: 'шар',
-      color: 'желтый',
-      size: 'большой',
-      favorite: false,
-    },
-    {
-      num: '20',
-      name: 'Красный шар с бантом',
-      count: '8',
-      year: '1950',
-      shape: 'шар',
-      color: 'красный',
-      size: 'средний',
-      favorite: true,
-    },
-    {
-      num: '21',
-      name: 'Красный шар с звёздами',
-      count: '4',
-      year: '1970',
-      shape: 'шар',
-      color: 'красный',
-      size: 'большой',
-      favorite: true,
-    },
-    {
-      num: '22',
-      name: 'Шишка еловая золотая',
-      count: '11',
-      year: '1990',
-      shape: 'шишка',
-      color: 'желтый',
-      size: 'малый',
-      favorite: false,
-    },
-    {
-      num: '23',
-      name: 'Колокольчик старинный',
-      count: '9',
-      year: '1950',
-      shape: 'колокольчик',
-      color: 'желтый',
-      size: 'большой',
-      favorite: false,
-    },
-    {
-      num: '24',
-      name: 'Снежинка изящная',
-      count: '1',
-      year: '1940',
-      shape: 'снежинка',
-      color: 'белый',
-      size: 'большой',
-      favorite: false,
-    },
-    {
-      num: '25',
-      name: 'Розовый шар с блёстками',
-      count: '12',
-      year: '2010',
-      shape: 'шар',
-      color: 'красный',
-      size: 'большой',
-      favorite: false,
-    },
-    {
-      num: '26',
-      name: 'Рубиново-золотой шар',
-      count: '8',
-      year: '1960',
-      shape: 'шар',
-      color: 'желтый',
-      size: 'большой',
-      favorite: false,
-    },
-    {
-      num: '27',
-      name: 'Красный шар с узором',
-      count: '4',
-      year: '2010',
-      shape: 'шар',
-      color: 'красный',
-      size: 'большой',
-      favorite: false,
-    },
-    {
-      num: '28',
-      name: 'Бордовый шар с узором',
-      count: '10',
-      year: '2010',
-      shape: 'шар',
-      color: 'красный',
-      size: 'большой',
-      favorite: false,
-    },
-    {
-      num: '29',
-      name: 'Старинный шар с цветами',
-      count: '5',
-      year: '1950',
-      shape: 'шар',
-      color: 'желтый',
-      size: 'большой',
-      favorite: true,
-    },
-    {
-      num: '30',
-      name: 'Старинный шар с узором',
-      count: '8',
-      year: '1950',
-      shape: 'шар',
-      color: 'желтый',
-      size: 'большой',
-      favorite: true,
-    },
-
-  ]; */
-
-class Products {
     render() {
         let htmlCatalog = '';
         data.forEach(({num, name, count, year, shape, color, size, favorite}) => {
@@ -445,7 +115,7 @@ class Products {
             <span>${color}</span>
             <span>${size}</span>
             <span>${favorite}</span>
-            <button>Добавить в корзину</button>
+            <button class = "products-element__btn">Добавить в корзину</button>
                         </li> `;
 
 
@@ -462,4 +132,6 @@ class Products {
 const productsPage = new Products();
 productsPage.render();
 
-/* ------------------------------------------------------------- */
+/* ----------------------------Активные кнопки для карточек--------------------------------- */
+
+
